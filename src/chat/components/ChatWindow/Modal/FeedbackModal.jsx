d9e1e8050ed996ee2904f30fc2a6878feb432576
Pompey06 <dmytrouryvskyi@gmail.com>
2026-03-19 00:02:13 +0100

    fix categories

--- src/chat/components/ChatWindow/Modal/FeedbackModal.jsx
+++ src/chat/components/ChatWindow/Modal/FeedbackModal.jsx
@@ -32,11 +32,11 @@ export default function FeedbackModal({
       label: t("feedback.reasons.wrong_quantity"),
     },
     {
-      value: "breach_of_confidentiality",
+      value: "confidentiality_issue",
       label: t("feedback.reasons.breach_of_confidentiality"),
     },
     {
-      value: "wrong_nace",
+      value: "wrong_classifier",
       label: t("feedback.reasons.wrong_nace"),
     },
     { value: "other", label: t("feedback.reasons.other") },
